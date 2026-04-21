23fad62ba313eb6ab840ce480abba6e58427e162
yoapapayosuke-max <yoapapayosuke@gmail.com>
2026-04-21 17:00:22 +0900

    新版

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'concrete-gishi-v20260421-6';
+const CACHE_NAME = 'concrete-gishi-v20260421-7';
 
 const APP_SHELL = [
   './',
-  './index.html?v=20260421-6',
-  './quiz.html?v=20260421-6',
-  './result.html?v=20260421-6',
+  './index.html',
+  './quiz.html',
+  './result.html',
   './style.css?v=20260421-6',
   './app.js?v=20260421-6',
   './questions.js?v=20260421-6',
   './mock-exam.js?v=20260421-6',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -49,7 +51,7 @@ self.addEventListener('fetch', (event) => {
           });
           return response;
         })
-        .catch(() => caches.match('./index.html?v=20260421-6'));
+        .catch(() => caches.match('./index.html'));
     })
   );
 });
