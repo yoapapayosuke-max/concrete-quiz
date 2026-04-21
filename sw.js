947847fc44c006485a75895aa05e4ce1e4fdc91c
yoapapayosuke-max <yoapapayosuke@gmail.com>
2026-04-22 06:24:32 +0900

    android

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = 'concrete-gishi-v20260421-7';
+const CACHE_NAME = 'concrete-gishi-v20260421-8';
 
 const APP_SHELL = [
-  './',
-  './index.html',
-  './quiz.html',
-  './result.html',
-  './style.css?v=20260421-6',
-  './app.js?v=20260421-6',
-  './questions.js?v=20260421-6',
-  './mock-exam.js?v=20260421-6',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
+  '/icon/',
+  '/icon/index.html',
+  '/icon/quiz.html',
+  '/icon/result.html',
+  '/icon/style.css?v=20260421-6',
+  '/icon/app.js?v=20260421-6',
+  '/icon/questions.js?v=20260421-6',
+  '/icon/mock-exam.js?v=20260421-6',
+  '/icon/manifest.webmanifest',
+  '/icon/icon-192.png',
+  '/icon/icon-512.png'
 ];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
